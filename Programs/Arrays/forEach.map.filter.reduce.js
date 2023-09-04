bf9bb3991b30ef0ee7396caefd89194b6a800bbc
Uday Tiwari <udaytiwari360@outlook.com>
2023-09-04 22:36:29 +0530

    Arrays: filter(), reduce()

--- Programs/Arrays/forEach.map.filter.reduce.js
+++ Programs/Arrays/forEach.map.filter.reduce.js
@@ -4,13 +4,13 @@ const arr = [1, 2, 3, "fizz", "buzz", 4, 5, "Interview.js"];
 
 // forEach() -> Higher-Order-Function : Takes arr function and calls it over every element over the array it is called!
 
-let forEach = arr.forEach(function(x) {
+let forEach = arr.forEach(function (x) {
     if (typeof x === "string") {
         console.log(NaN);
     } else if (x % 2 === 0) {
         console.log(x);
     }
-}); 
+});
 
 console.log(forEach); // undefined
 
@@ -22,7 +22,7 @@ console.log(forEach); // undefined
  NaN
 */
 
-// map() -> Higher-Order-Function : Takes arr function and calls it over every element of the array and throws an new array out of it!
+// map() -> Higher-Order-Function : Takes a function and calls it over every element of the array and throws an new array out of it!
 
 const output = arr.map(x => typeof x);
 
@@ -37,3 +37,28 @@ console.log(output);
 ]
 */
 
+const users = [
+    { firstName: "Uday", LastName: "Tiwari", age: 19 },
+    { firstName: "Akshay", LastName: "Saini", age: 29 },
+];
+
+const fullNames = users.map(x => x.firstName + " " + x.LastName); // [ 'Uday Tiwari', 'Akshay Saini' ]
+
+console.log(fullNames);
+
+// filter() -> Higher-Order-Function : Takes a function to filter out specific elements in the given array and throws an new array out of it!
+
+const onlyStrings = arr.filter(x => typeof x === "string");
+
+console.log(onlyStrings); // [ 'fizz', 'buzz', 'Interview.js' ]
+
+// reduce() -> reduces an array and accumulation of value(s), and is provided as an argument in the next call to the callback function!
+
+const total = arr.reduce(function (acc, curr) {
+    if (typeof curr === "number") {
+        acc = acc + curr;
+    }
+    return acc;
+}, 0);
+
+console.log(total);
